702ed182f8b2ca3d1d68139564b8689023062631
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-08-12 21:00:00 +0600

    Add reset password route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import { testRoute } from './testRoute';
 import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmailRoute';
 import { forgotPasswordRoute } from './forgotPasswordRoute';
+import { resetPasswordRoute } from './resetPasswordRoute';
 
 export const routes = [
   logInRoute,
@@ -12,4 +13,5 @@ export const routes = [
   updateUserInfoRoute,
   verifyEmailRoute,
   forgotPasswordRoute,
+  resetPasswordRoute,
 ];

b0c16975bcd9577dd0fc20cb343a5cdf429198a0
Yuriy Vyatkin <yuriyvyatkin@yandex.ru>
2023-08-13 20:13:44 +0600

    Implement Google auth

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,8 @@ import { updateUserInfoRoute } from './updateUserInfoRoute';
 import { verifyEmailRoute } from './verifyEmailRoute';
 import { forgotPasswordRoute } from './forgotPasswordRoute';
 import { resetPasswordRoute } from './resetPasswordRoute';
+import { getGoogleOauthUrlRoute } from './getGoogleOauthUrlRoute';
+import { googleOauthCallbackRoute } from './googleOauthCallbackRoute';
 
 export const routes = [
   logInRoute,
@@ -14,4 +16,6 @@ export const routes = [
   verifyEmailRoute,
   forgotPasswordRoute,
   resetPasswordRoute,
+  getGoogleOauthUrlRoute,
+  googleOauthCallbackRoute,
 ];
